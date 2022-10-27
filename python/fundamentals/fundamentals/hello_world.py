83368c5e77a2f9d8b15df96d6ecedfef5441ae9b
svonrickenbach <samrickenbach@gmail.com>
2022-10-27 13:49:08 -0700

    update

--- python/fundamentals/fundamentals/hello_world.py
+++ python/fundamentals/fundamentals/hello_world.py
@@ -1,5 +1,15 @@
-x = "Hello Python"
-print(x)
-y = 42
-print(y)
+from tokenize import Name
 
+
+print("Hello World")
+name = "Noelle"
+print("Hello", name, "!") 
+print("Hello " + name + "!")
+name = 42
+print("Hello", name, "!")
+print("Hello " + str(name) + "!")
+
+fave_food1 = "sushi"
+fave_food2 = "pizza"
+print("I love to eat {} and {}.".format(fave_food1, fave_food2))
+print(f"I love to eat {fave_food1} and {fave_food2}.")
