2df08e7d99c15b33da3b868af199614f920df7fd
svonrickenbach <samrickenbach@gmail.com>
2022-10-27 15:57:17 -0700

    updates

--- python/fundamentals/fundamentals/for_loop_basic1.py
+++ python/fundamentals/fundamentals/for_loop_basic1.py
@@ -6,11 +6,26 @@
 #     print (x)
 #     x += 1
 
-# for i in range(5, 1001):
-#     if i % 5 == 0:
+# for i in range(5, 1001, 5):
 #         print(i)
 
-x=5 
-while x <= 1000:
-    print(x)
-    x += 5
+# x=5 
+# while x <= 1000:
+#     print(x
+#     x += 5
+
+# for i in range(1, 101):
+#     if i % 10 == 0:
+#         print("Coding Dojo")
+#     elif i % 5 == 0: 
+#         print("Coding")
+#     else: print(i)
+
+x = 1 
+while x <= 100: 
+    if x % 10 == 0: 
+        print("Coding Dojo")
+    elif x % 5 == 0:
+        print("Coding")
+    else: print(x)
+    x +=1 
