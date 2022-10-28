d301fe221b59aeaf1ff3db84c72d590a165ac46a
svonrickenbach <samrickenbach@gmail.com>
2022-10-28 00:36:36 -0700

    finished

--- python/fundamentals/fundamentals/for_loop_basic1.py
+++ python/fundamentals/fundamentals/for_loop_basic1.py
@@ -1,25 +1,25 @@
-# for i in range(0, 151):
-#     print(i)
+for i in range(0, 151):
+    print(i)
 
 # x = 0 
 # while x <= 150: 
-#     print (x)
+#     print(x)
 #     x += 1
 
-# for i in range(5, 1001, 5):
-#         print(i)
+for i in range(5, 1001, 5):
+        print(i)
 
 # x=5 
 # while x <= 1000:
-#     print(x
+#     print(x)
 #     x += 5
 
-# for i in range(1, 101):
-#     if i % 10 == 0:
-#         print("Coding Dojo")
-#     elif i % 5 == 0: 
-#         print("Coding")
-#     else: print(i)
+for i in range(1, 101):
+    if i % 10 == 0:
+        print("Coding Dojo")
+    elif i % 5 == 0: 
+        print("Coding")
+    else: print(i)
 
 # x = 1 
 # while x <= 100: 
@@ -30,15 +30,15 @@
 #     else: print(x)
 #     x +=1 
 
-# x=0
-# for i in range(1, 500001):
-#     if i % 2 != 0:
-#         # print(i) 
-#         x = x + i 
-# print(x)
+x=0
+for i in range(1, 500001):
+    if i % 2 != 0:
+        # print(i) 
+        x = x + i 
+print(x)
 
-# for i in range(2018, 0, -4):
-#     print(i)
+for i in range(2018, 0, -4):
+    print(i)
 
 # x = 2022
 # while x >= 4: 
@@ -46,7 +46,7 @@
 #     print(x)
 
 
-mult = 10
+mult = 5
 lowNum = 1
 highNum = 20 
 for i in range(lowNum, highNum + 1):
