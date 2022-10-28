7e82d3472915fc765931bdddfb0ca67e285ab466
svonrickenbach <samrickenbach@gmail.com>
2022-10-27 21:03:48 -0700

    completed q4

--- python/fundamentals/fundamentals/for_loop_basic1.py
+++ python/fundamentals/fundamentals/for_loop_basic1.py
@@ -21,11 +21,18 @@
 #         print("Coding")
 #     else: print(i)
 
-x = 1 
-while x <= 100: 
-    if x % 10 == 0: 
-        print("Coding Dojo")
-    elif x % 5 == 0:
-        print("Coding")
-    else: print(x)
-    x +=1 
+# x = 1 
+# while x <= 100: 
+#     if x % 10 == 0: 
+#         print("Coding Dojo")
+#     elif x % 5 == 0:
+#         print("Coding")
+#     else: print(x)
+#     x +=1 
+
+x=0
+for i in range(1, 500001):
+    if i % 2 != 0:
+        # print(i) 
+        x = x + i 
+print(x)
