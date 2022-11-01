68f0ebb93f275554a798474496130e7276015d99
svonrickenbach <samrickenbach@gmail.com>
2022-11-01 14:37:43 -0700

    added add_points method

--- python/fundamentals/oop/user.py
+++ python/fundamentals/oop/user.py
@@ -18,12 +18,12 @@ class User:
         print("--------------------")
         
     def enroll(self):
-        self.gold_card_points = 200
         if self.is_rewards_member == True: 
             print("User already a member.")
             return False
         else:
             self.is_rewards_member = True
+            self.gold_card_points = 200    
         return self.gold_card_points, self.is_rewards_member
     
     def spend_points(self, amount):
@@ -32,20 +32,28 @@ class User:
             return self.gold_card_points
         else: 
             print("Not Enough Points")
+    
+    def add_point(self, add):
+        self.gold_card_points += add
 
 sam_rickenbach = User("Sam", "Rickenbach", "samrickenbach@gmail.com", 28)
 kara_cooper = User("Kara", "Cooper", "Cooper@gmail.com", 33)
 sampson_middel = User("Sampson", "Middel", "Samoureye@gmail.com", 29)
 
 sam_rickenbach.enroll()
-kara_cooper.enroll()
-sampson_middel.enroll()
-sam_rickenbach.spend_points(50)
-kara_cooper.spend_points(80)
+# kara_cooper.enroll()
+# sampson_middel.enroll()
+# sam_rickenbach.spend_points(50)
+# kara_cooper.spend_points(80)
 
 sam_rickenbach.enroll()
 
 sam_rickenbach.display_info()
-kara_cooper.display_info()
-sampson_middel.spend_points(240)
-sampson_middel.display_info()
+# kara_cooper.display_info()
+# sampson_middel.spend_points(240)
+# sampson_middel.display_info()
+
+sam_rickenbach.add_point(50)
+sam_rickenbach.enroll()
+
+sam_rickenbach.display_info()
