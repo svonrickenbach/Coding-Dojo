e58acb6b29490344eae1afd58ddba524f6209fd7
svonrickenbach <samrickenbach@gmail.com>
2022-11-10 16:41:56 -0800

    finished assignment

--- python/flask/fundamentals/playground/server.py
+++ python/flask/fundamentals/playground/server.py
@@ -3,7 +3,15 @@ app = Flask(__name__)
 
 @app.route('/play')
 def play():
-    return render_template("index.html")
+    return render_template('index.html', times=3)
+
+@app.route('/play/<times>')
+def repeat(times):
+    return render_template('index.html', times = int(times))
+
+@app.route('/play/<times>/<color>')
+def color(times, color):
+    return render_template('index.html', times = int(times), color = color)
 
 if __name__=="__main__":
     app.run(port=5001, debug = True)
