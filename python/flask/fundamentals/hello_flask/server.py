023d7cc240379315b504b3e8a6c68175c8147f52
svonrickenbach <samrickenbach@gmail.com>
2022-11-14 21:00:54 -0800

    updated

--- python/flask/fundamentals/hello_flask/server.py
+++ python/flask/fundamentals/hello_flask/server.py
@@ -20,6 +20,16 @@ def show_user_profile(username, id):
     print(id)
     return "username: " + username + ", id: " + id
 
+@app.route('/lists')
+def render_lists():
+    student_info = [
+        {'name' : 'Michael', 'age' : 35},
+        {'name' : 'John', 'age' : 30},
+        {'name' : 'Mark', 'age' : 25},
+        {'name' : 'KB', 'age' : 27},
+    ]
+    return render_template("lists.html", random_numbers = [3,1,5], students = student_info)
+
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
