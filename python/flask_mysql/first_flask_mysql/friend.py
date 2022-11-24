1b71ae3891e224c145b075bf14e82d9cd749d602
svonrickenbach <samrickenbach@gmail.com>
2022-11-23 20:40:19 -0800

    demonstration started, looped through data in db

---
+++ python/flask_mysql/first_flask_mysql/friend.py
@@ -0,0 +1,26 @@
+from mysqlconnection import connectToMySQL
+
+class Friend: 
+    def __init__(self, data):
+        self.id = data['id']
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
+        self.occupation = data['occupation']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+
+    @classmethod
+    def get_all(cls):
+        query = "SELECT * FROM friends;"
+        results = connectToMySQL('first_flask').query_db(query)
+        print(results)
+        friends = []
+        for i in results:
+            print(i)
+            friends.append(cls(i))
+        return friends 
+
+    @classmethod 
+    def save(cls, data):
+        query = "INSERT INTO friends (first_name, last_name, occupation, created_at, updated_at VALUES (%(fname)s, %(lname)s, %(occ)s, NOW(), NOW() );"
+        return connectToMySQL('first_flask').query_db(query, data)
