8afb28fad9d93dab47c5d0f734301a8d0f664f77
svonrickenbach <samrickenbach@gmail.com>
2022-12-10 11:50:23 -0800

    removed

--- python/flask_mysql/crud/belt_exam/server.py
+++
@@ -1,5 +0,0 @@
-from flask_app.controllers import login_reg_controller, user_controller, sighting_controller, skeptic_controller
-from flask_app import app
-
-if __name__=='__main__':
-    app.run(port=5001, debug=True)
