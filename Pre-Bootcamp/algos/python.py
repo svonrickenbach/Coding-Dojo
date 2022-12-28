051d399391a49abfc5ef15a22f0d55ec96df73a5
svonrickenbach <samrickenbach@gmail.com>
2022-12-28 13:12:27 -0800

    installed django

---
+++ Pre-Bootcamp/algos/python.py
@@ -0,0 +1,31 @@
+
+# def solveMeFirst(a,b):
+# 	# Hint: Type return a+b below
+#     if a <= 1000: 
+#         if b <= 1000:
+#             return a + b 
+
+# num1 = int(2)
+# num2 = int(3)
+# res = solveMeFirst(num1,num2)
+# print(res)
+
+
+def simpleArraySum(ar):
+    # Write your code here
+    sum = 0
+    for i in range(ar_count):
+        sum = sum + ar[i]
+
+    return sum 
+
+ar_count = 6
+print(simpleArraySum([1, 2, 3, 4, 10, 11]))
+
+
+def compareTriplets(a, b):
+    # Write your code here
+
+
+a = [1, 2, 3] 
+b = [3, 2, 1] 
---
+++ python/django/my_environments/djangoPy3Env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/samrickenbach/Documents/Coding-Dojo/python/django/my_environments/djangoPy3Env/bin/python3.11
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
