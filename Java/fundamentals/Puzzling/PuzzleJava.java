01981a58f799d4c63bfd4873836fd938e18111b7
svonrickenbach <samrickenbach@gmail.com>
2023-01-18 22:13:09 -0800

    PuzzleJava done except for sensei bonus

--- Java/fundamentals/Puzzling/PuzzleJava.java
+++ Java/fundamentals/Puzzling/PuzzleJava.java
@@ -1,6 +1,6 @@
 import java.util.Random;
 import java.util.ArrayList;
-
+import java.util.Arrays;
 public class PuzzleJava {
     Random randMachine = new Random(); 
 
@@ -12,5 +12,31 @@ public class PuzzleJava {
         return random10;
     }
 
-    public char 
+    public char getRandomLetter() {
+        char[] myArray = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+        return myArray[randMachine.nextInt(25)];
+        }
+
+    public String generatePassword() {
+        String password = "";
+        for (int i = 0; i <= 7; i++) {
+            password = password + String.format("%c", getRandomLetter());
+        }
+        return password;
+    }
+
+    public String[] getNewPasswordSet(int num) {
+        String[] myArray;
+        myArray = new String[num];
+        for (int i = 0; i < num; i++) {
+            String password = "";
+            for (int y = 0; y <= 7; y++) {
+                password = password + String.format("%c", getRandomLetter());
+            }
+            myArray[i] = password;
+        }
+        return myArray;
+    }
 }
+
+
--- Java/fundamentals/Puzzling/TestPuzzle.java
+++ Java/fundamentals/Puzzling/TestPuzzle.java
@@ -1,12 +1,27 @@
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.Arrays;
+
 public class TestPuzzle {
     
 	public static void main(String[] args) {
 		PuzzleJava generator = new PuzzleJava();
+
+		System.out.println("-------Test Print Random #-------\n");
 		ArrayList<Integer> randomRolls = generator.getTenRolls();
 		System.out.println(randomRolls);
+
+		System.out.println("\n-------Test Print Random Letter-------\n");
+		char letter = generator.getRandomLetter();
+		System.out.println(letter + "\n");
+
+		System.out.println("\n-------Test Print Password-------\n");
+		String password = generator.generatePassword();
+		System.out.println(password + "\n");
 		
+		System.out.println("\n-------Test Print Password Array-------\n");
+		String[] passwordArray = generator.getNewPasswordSet(4);
+		System.out.println(Arrays.toString(passwordArray) + "\n");
     	//..
 		// Write your other test cases here.
 		//..
