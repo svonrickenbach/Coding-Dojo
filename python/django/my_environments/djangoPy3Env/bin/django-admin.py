12b45cbb3ff12cd9d44fe188270ef6f651f52b54
Sam Rickenbach <113722199+svonrickenbach@users.noreply.github.com>
2023-01-24 15:22:48 -0800

    Delete python/django directory
    It was too large

--- python/django/my_environments/djangoPy3Env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/samrickenbach/Documents/Coding-Dojo/python/django/my_environments/djangoPy3Env/bin/python3.11
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
