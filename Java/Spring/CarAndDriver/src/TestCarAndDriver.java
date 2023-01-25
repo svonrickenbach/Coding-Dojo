f6f8b7ea78a11fbf0ddaf6e54d822b92d9822b22
svonrickenbach <samrickenbach@gmail.com>
2023-01-24 23:01:53 -0800

    fixed

--- Java/Spring/CarAndDriver/src/TestCarAndDriver.java
+++ Java/Spring/CarAndDriver/src/TestCarAndDriver.java
@@ -4,33 +4,33 @@ public class TestCarAndDriver {
 	public static void main(String[] args) {
 		Driver driver1 = new Driver();
 		
-////		Assignment Test Conditions
+//		Assignment Test Conditions
+		driver1.driving();
+		driver1.driving();
+		driver1.driving();
+		driver1.driving();
+		driver1.booster();
+		driver1.refuel();
+		driver1.refuel();
+		driver1.refuel();
+		
+//		Bonus Challenge Conditions 
+//		driver1.driving();
 //		driver1.driving();
 //		driver1.driving();
 //		driver1.driving();
 //		driver1.driving();
 //		driver1.booster();
+//		driver1.booster();
 //		driver1.refuel();
 //		driver1.refuel();
 //		driver1.refuel();
-		
-//		Bonus Challenge Conditions 
-		driver1.driving();
-		driver1.driving();
-		driver1.driving();
-		driver1.driving();
-		driver1.driving();
-		driver1.booster();
-		driver1.booster();
-		driver1.refuel();
-		driver1.refuel();
-		driver1.refuel();
-		driver1.refuel();
-		driver1.driving();
-		driver1.refuel();
-		driver1.booster();
-		driver1.booster();
-		driver1.booster();
+//		driver1.refuel();
+//		driver1.driving();
+//		driver1.refuel();
+//		driver1.booster();
+//		driver1.booster();
+//		driver1.booster();
 
 		
 
