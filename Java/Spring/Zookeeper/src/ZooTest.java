8c2a0f1faa6c7e91218a6b58533bdd7e71d281b4
svonrickenbach <samrickenbach@gmail.com>
2023-01-24 23:51:56 -0800

    Zookeeper Started

---
+++ Java/Spring/Zookeeper/src/ZooTest.java
@@ -0,0 +1,20 @@
+
+public class ZooTest {
+
+	public static void main(String[] args) {
+//		Mammal mammal1 = new Mammal();
+//		mammal1.displayEnergy();
+		
+		Gorilla gorilla1 = new Gorilla();
+		gorilla1.displayEnergy();
+		
+		gorilla1.throwThings();
+		gorilla1.throwThings();
+		gorilla1.throwThings();
+		gorilla1.eatBananas();
+		gorilla1.eatBananas();
+		gorilla1.climb();
+		gorilla1.displayEnergy();
+	}
+
+}
