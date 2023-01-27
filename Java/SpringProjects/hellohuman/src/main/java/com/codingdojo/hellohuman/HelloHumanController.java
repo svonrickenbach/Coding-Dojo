5dbe6c122ab855ad0771d4073a13f4ffbab659c1
svonrickenbach <samrickenbach@gmail.com>
2023-01-27 13:15:41 -0800

    HelloHuman Assignment Finished

--- Java/SpringProjects/hellohuman/src/main/java/com/codingdojo/hellohuman/HelloHumanController.java
+++ Java/SpringProjects/hellohuman/src/main/java/com/codingdojo/hellohuman/HelloHumanController.java
@@ -9,12 +9,27 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloHumanController {
 	
 	@RequestMapping("/")
-	public String index(@RequestParam(value="q", required=false) String searchQuery) {
-		if(searchQuery == null) {
-			return "Hello Human";
+	public String index(@RequestParam(value="name", required=false) String searchQuery, @RequestParam(value="last_name", required= false) String searchQuery1, @RequestParam(value="times", required= false) Integer multiplier) {
+		if(searchQuery1 == null && searchQuery == null && multiplier != null) {
+			String string = "Hello Human ";
+			return string.repeat(multiplier);
+		}
+		else if (searchQuery1 == null && searchQuery == null && multiplier == null){
+				return "Hello Human";
+		}
+		else if(searchQuery1 == null && multiplier == null) {
+			return "Hello " + searchQuery;
+		}
+		else if(searchQuery1 == null && multiplier != null) {
+			String string = String.format("Hello %s ", searchQuery);
+			return string.repeat(multiplier);
+		}
+		else if(searchQuery1 != null && searchQuery != null && multiplier != null){
+			String string = String.format("Hello %s %s ", searchQuery, searchQuery1);
+			return string.repeat(multiplier);
 		}
 		else {
-			return "Hello " + searchQuery;
+			return "Hello " + searchQuery + " " + searchQuery1;
 		}
 	}
 }
