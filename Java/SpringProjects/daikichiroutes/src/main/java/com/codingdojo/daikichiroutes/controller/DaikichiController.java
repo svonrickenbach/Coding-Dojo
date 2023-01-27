053f9596b7b482254f56f20f083f90235738ba8f
svonrickenbach <samrickenbach@gmail.com>
2023-01-26 19:48:32 -0800

    updated class level annotation

--- Java/SpringProjects/daikichiroutes/src/main/java/com/codingdojo/daikichiroutes/controller/DaikichiController.java
+++ Java/SpringProjects/daikichiroutes/src/main/java/com/codingdojo/daikichiroutes/controller/DaikichiController.java
@@ -4,19 +4,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/daikichi")
 public class DaikichiController {
 	
-	@RequestMapping("/daikichi")
+	@RequestMapping("")
 	public String welcome() {
 		return "Welcome!";
 	}
 	
-	@RequestMapping("/daikichi/today")
+	@RequestMapping("/today")
 	public String today() {
 		return "Today you will find luck in all your endeavors!!";
 	}
 	
-	@RequestMapping("/daikichi/tomorrow")
+	@RequestMapping("/tomorrow")
 	public String tomorrow() {
 		return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
 	}
