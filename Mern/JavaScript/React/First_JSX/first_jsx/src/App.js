77b32a720b8c44f3b26622317f0d51a40c779c8a
svonrickenbach <samrickenbach@gmail.com>
2023-03-15 19:45:37 -0700

    first jsx complete

---
+++ Mern/JavaScript/React/First_JSX/first_jsx/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>Hello Dojo!</h1>
+      <h2>Things I need to do:</h2>
+      <ul>
+        <li>Learn React</li>
+        <li>Climb Mt. Everest</li>
+        <li> Run a marathon</li>
+        <li>Feed the dogs</li>
+      </ul>
+    </>
+  );
+}
+
+export default App;
