42883b4dca0183dd2714327e354f4c8c5615ea09
svonrickenbach <samrickenbach@gmail.com>
2023-03-31 00:45:36 -0700

    done with axios pokemon api

---
+++ Mern/JavaScript/React/axios_pokemon_api/src/App.js
@@ -0,0 +1,37 @@
+import {useState, useEffect} from 'react'
+import axios from 'axios'
+import './App.css';
+
+function App() {
+  
+  const [pokemon, setPokemon] = useState([])
+
+  useEffect(() => {
+    axios.get('https://pokeapi.co/api/v2/pokemon?limit=808&offset=0')
+    .then((response) => {
+      console.log(`Response: ${response}`);
+      console.log(response);
+      setPokemon(response.data.results)
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+  }, [])
+
+  // console.log(pokemon());
+
+  return (
+    <div className="App">
+      <h1>Pokemon API</h1>
+      {
+        pokemon.map((character, idx) => (
+            <h2>
+              {idx}. {character.name}
+            </h2>
+        ))
+        }
+    </div>
+  );
+}
+
+export default App;
--- Mern/JavaScript/React/test.js
+++ Mern/JavaScript/React/test.js
@@ -10,3 +10,15 @@ import React, { useState } from 'react';
 // });
 // setState({ items: state.items, totalPrice: state.totalPrice + 10.99 });
 // console.log(state.items);
+
+const noMondays = new Promise( (resolve, reject) => {
+    if(new Date().getDay() !== 4) {
+        resolve("Good, it's not Monday!");
+    } else {
+        reject("Someone has a case of the Mondays!");
+    }
+    console.log(new Date().getDay());
+});
+noMondays
+    .then( res => console.log(res) )
+    .catch( err => console.log(err) );
